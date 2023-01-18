8620d55288a78f64372e69450728aaa2245ed36e
Arman <real.alzhanov@gmail.com>
2023-01-18 21:37:57 +0600

    Initial Commit
    Create a custom Odoo 13.0 module.

---
+++ __manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "test for QZHub",
+
+    'summary': """
+        This module was created for the QZHub task""",
+
+    'description': """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Maecenas cursus varius ipsum. Proin vestibulum sem tortor, id cursus sapien elementum gravida. 
+        Morbi ultricies velit vel orci aliquet elementum. In tristique leo pretium, gravida nisi egestas, tempor enim. 
+        Vivamus sed eros lectus. Donec tristique condimentum ipsum, in venenatis urna tristique sed. 
+        In rhoncus tortor vitae lacus commodo suscipit.
+    """,
+
+    'author': "Arman",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    
+    'application': True
+}
